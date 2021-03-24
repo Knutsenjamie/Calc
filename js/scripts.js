102d33ae8525079e87d585172e8c4144b9664f4a
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-03-24 14:08:17 -0700

    add more functionality to jQuery
    Co-authored-by: Ryan Walker <ryandanielwalker@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,29 @@
 function add(number1, number2) {
   return number1 + number2;
-}
+};
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+function subtract(number1, number2) {
+  return number1 - number2;
+};
+
+function multiply(number1, number2) {
+  return number1 * number2;
+};
+
+function divide(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    // const number1 = parseInt($("#minus1").val());
+    // const number2 = parseInt($("#minus2").val());
+    // const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});  
+
